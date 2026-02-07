138dd213a6682498aa0b2bc4652dfb538bce19f2
rajveerroy74 <rajveerkohi@gmail.com>
2026-02-07 11:40:33 +0530

    Initial push

--- src/main/java/in/rajveer/blooms/dto/UserRequest.java
+++ src/main/java/in/rajveer/blooms/dto/UserRequest.java
@@ -8,7 +8,8 @@ public class UserRequest {
     private String profileUrl;
     private String phoneNumber;
 
-    public UserRequest(String username, String email, String name, String password, String profileUrl, String phoneNumber) {
+    public UserRequest(String username, String email, String name, String password, String profileUrl,
+            String phoneNumber) {
         this.username = username;
         this.email = email;
         this.name = name;
@@ -17,11 +18,56 @@ public class UserRequest {
         this.phoneNumber = phoneNumber;
     }
 
+    public UserRequest() {
+    }
+
     // Getters
-    public String getUsername() { return username; }
-    public String getEmail() { return email; }
-    public String getName() { return name; }
-    public String getPassword() { return password; }
-    public String getProfileUrl() { return profileUrl; }
-    public String getPhoneNumber() { return phoneNumber; }
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
